599454064d65595f3140f7d60ed958978fe5a062
Selvabharathi05 <111355346+Selvabharathi05@users.noreply.github.com>
2022-08-24 19:40:04 +0530

    Create Problem2.cs

---
+++ 24-8-2022/Problem2.cs
@@ -0,0 +1,43 @@
+
+Problem 2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReusableCodeLibrary;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String");
+            string s1 = Console.ReadLine();
+            StringCopy.Stringcopy(s1);
+
+            Console.ReadLine();
+        }
+    }
+}
+==========================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class StringCopy
+    {
+        public static void Stringcopy( string s1)
+        {
+            var s = string.Copy(s1);
+            Console.WriteLine("Copied String" + " "+ s);
+            
+        }
+    }
+}
