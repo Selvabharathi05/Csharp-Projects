13db890e450038e421f9b571213ff8c742d9b8b3
Selvabharathi05 <111355346+Selvabharathi05@users.noreply.github.com>
2022-08-24 13:29:09 +0530

    Create Problem5.cs

---
+++ 23-8-2022/Problem5.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Problem_5
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Employee[]  Employees = new Employee[2];
+            for (int i = 0; i < 2 ; i++)
+            {
+                
+                Console.WriteLine("Enter Employee Number");
+                int empno =Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Employee Name");
+                string ename = Console.ReadLine();
+                Console.WriteLine("Enter Employee Salary");
+                int sal = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Department Number");
+                int deptno = Convert.ToInt32(Console.ReadLine());
+                Employees[i].Acceptdata(empno, ename, sal, deptno);
+                Console.WriteLine("-------------------------------------------");
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Employees[i].Display();
+                Console.WriteLine("------------------------------------------------");
+               
+            }
+            Console.ReadLine();
+        }
+    }
+   
+}
+
+
+=======================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Problem_5
+{
+    internal class Class1
+    {
+    }
+    struct Employee
+    {
+        int _empno;
+        string _ename;
+        int _sal;
+        int _deptno;
+
+        public void Acceptdata(int empno, string ename, int sal, int deptno)
+        {
+            this._empno = empno;
+            this._ename = ename;
+            this._sal = sal;
+            this._deptno = deptno;
+        }
+        public void Display()
+        {
+            Console.WriteLine(_empno);
+            Console.WriteLine(_ename);
+            Console.WriteLine(_sal);
+            Console.WriteLine(_deptno);
+        }
+
+    }
+}
