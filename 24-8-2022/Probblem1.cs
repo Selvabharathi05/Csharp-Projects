66f3c934ebbcc3c7c8a2f96cbe5e35d38a08d5b2
Selvabharathi05 <111355346+Selvabharathi05@users.noreply.github.com>
2022-08-24 19:39:21 +0530

    Create Probblem1.cs

---
+++ 24-8-2022/Probblem1.cs
@@ -0,0 +1,50 @@
+Problem 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReusableCodeLibrary;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First String");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Enter Second String");
+            string s2 = Console.ReadLine();
+            StringCompare.stringcompare(s1, s2);
+            
+            Console.ReadLine();
+        }
+    }
+}
+===================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class StringCompare
+    {
+        public static void stringcompare(string s1, string s2)
+        {
+            
+            if (s1 == s2)
+            {
+                Console.WriteLine("Strings are equal");
+            }
+            else
+            {
+                Console.WriteLine("Strings are not equal");
+            }
+        }
+    }
+}
