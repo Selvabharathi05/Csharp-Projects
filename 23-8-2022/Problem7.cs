02c46464f4c1dfa44d4f5fe770376eccfa373771
Selvabharathi05 <111355346+Selvabharathi05@users.noreply.github.com>
2022-08-24 13:30:52 +0530

    Create Problem7.cs

---
+++ 23-8-2022/Problem7.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Problem_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int j = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int k = i / j;
+                Console.WriteLine(k);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("This is Divided by Zero Exception");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
