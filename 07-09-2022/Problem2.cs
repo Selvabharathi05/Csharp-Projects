79cc20c96df497b6121e8b769defee70aa2ce398
Selvabharathi05 <111355346+Selvabharathi05@users.noreply.github.com>
2022-09-07 17:30:14 +0530

    Create Problem2.cs

---
+++ 07-09-2022/Problem2.cs
@@ -0,0 +1,44 @@
+using greetuser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7sep_Problem2
+{
+    public delegate void guser(string s);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name");
+            string s = Console.ReadLine(); 
+            Greet greet = new Greet();
+            guser g = new guser(greet.greetinguser);
+            g(s);
+            Console.ReadKey();
+
+        }
+    }
+}
+===============================================================
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greetuser
+{
+    public class Greet
+    {
+        public void greetinguser(string s)
+        {
+            Console.WriteLine("Hello {0} Welcome To C#",s );
+
+        }
+    }
+}
